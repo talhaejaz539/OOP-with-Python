ce73a6faf26c47faf313621cb58d19d17a94fff3
talhaejaz539 <talhaejazup@gmail.com>
2022-10-01 23:04:39 +0500

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,36 @@
+# Python OOP
+
+from distutils.debug import DEBUG
+
+
+class Employee:
+
+    num_of_employees = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_employees += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+class Developer(Employee):
+    pass
+
+
+dev_1 = Developer('Talha', 'Ejaz', 50000)
+dev_2 = Developer('Test', 'User', 60000)
+
+print(help(Developer))
+
+# print(dev_1.email)
+# print(dev_2.email)
