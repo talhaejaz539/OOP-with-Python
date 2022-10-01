499002f0fae6fca47ade2c95d8c1957db7a6d3a5
talhaejaz539 <talhaejazup@gmail.com>
2022-10-01 13:32:07 +0500

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 # Python OOP
+
 class Employee:
 
     num_of_employees = 0
@@ -22,10 +23,20 @@ class Employee:
     def set_raise_amount(cls, amount):
         cls.raise_amount = amount
 
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
 
-print(Employee.num_of_employees)
 
 emp_1 = Employee('Talha', 'Ejaz', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
-print(Employee.num_of_employees)
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
