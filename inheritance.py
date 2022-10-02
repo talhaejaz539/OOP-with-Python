623db8081b52a0bdca339df297e18e8719297838
talhaejaz539 <talhaejazup@gmail.com>
2022-10-02 23:10:44 +0500

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -23,13 +23,52 @@ class Employee:
 class Developer(Employee):
     raise_amount = 1.10
 
+    def __init__(self, first, last, pay, prog_lang):
+        # Employee.__init__(self, first, last, pay)
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-dev_1 = Employee('Talha', 'Ejaz', 50000)
-dev_2 = Developer('Test', 'User', 60000)
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        # Employee.__init__(self, first, last, pay)
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Talha', 'Ejaz', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(issubclass(Manager, Developer))
+
+# print(mgr_1.email)
+
+# mgr_1.add_emp(dev_2)
+# mgr_1.remove_emp(dev_1)
+
+# mgr_1.print_emps()
 
 # print(dev_1.email)
-# print(dev_2.email)
+# print(dev_1.prog_lang)
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
